988b3d6da2d8d7daf4e1b5e8925f15a062c4e248
Bartłomiej Pawlik <bartlomiejpawlik0@gmail.com>
2022-06-05 11:51:50 +0200

    Add class Measurements with selectors

--- page_objects/measurement_page.py
+++ page_objects/measurement_page.py
@@ -5,7 +5,7 @@ from time import sleep
 from selenium.webdriver.common.action_chains import ActionChains
 
 class Variables():
-    #POWERSTATION FORM VARIABLES
+    # POWERSTATION FORM VARIABLES
     powerstation_name = "Test Powerstation"
     powerstation_street = "Test street"
     powerstation_street_number = "1"
@@ -16,7 +16,7 @@ class Variables():
     powerstation_phone = "000000001"
     powerstation_fax = "123456789"
     powerstation_email = "testmail@gmail.com"
-    #GENERATOR FORM VARIABLES
+    # GENERATOR FORM VARIABLES
     generator_name = "Testgen1"
     generator_manufacturer = "Testing Company"
     generator_type = "GTH360"
@@ -33,5 +33,64 @@ class Variables():
     generator_cos = "0.85"
     generator_speed = "3000"
     generator_cooling = "hydrogen"
-    #Measurement Page Variables
+    # Measurement Page Variables
     measurement_date = "2020-02-02"
+
+class Measurement():
+    # All selectors used in these methods
+    # Find by LINK or Partial LINK
+    measurement_page_main = "Pomiary"
+    measurement_page_left_sidebar = "Pomiary"
+    powerstation_page = "Elektrownie"
+    generator_page = "Generatory"
+    # Find by  ID
+    Login = "login"
+    Password = "password"
+    LoginBTN = "Login"
+    measurement_add_btn = "submit"
+    measurement_select_generator_form = "msaddform_generator"
+    measurement_select_datepicker = "measurement_date"
+    measurement_table = "measurement_table"
+    powerstation_name = "powerstation_name"
+    powerstation_street = "powerstation_street"
+    powerstation_street_number = "powerstation_streetnum"
+    powerstation_building_number = "powerstation_localnum"
+    powerstation_zip_code = "powerstation_zipcode"
+    powerstation_city = "powerstation_city"
+    powerstation_select = "genform_powerstation_name"
+    generator_name = "genform_generator_name"
+    generator_manufacturer = "producer"
+    generator_type = "device_type"
+    generator_serialno = "manufacture_no"
+    generator_year = "manufacture_year"
+    generator_rated_voltage = "rated_voltage"
+    generator_rated_current = "rated_current"
+    generator_rated_excitation_voltage = "excitation_voltage"
+    generator_rated_excitation_current = "excitation_current"
+    generator_insulation = "insulation_type"
+    generator_power_apparent = "generator_power_mva"
+    generator_power_real = "generator_power_mw"
+    generator_power_reactive = "generator_power_mvar"
+    generator_cos = "cos_fi"
+    generator_speed = "rotor_speed"
+    generator_cooling = "cooling"
+    generator_table = "measurement_table"
+    # Select country
+    powerstation_country = "powerstation_city"
+    powerstation_phone = "powerstation_telephone"
+    powerstation_fax = "powerstation_fax"
+    powerstation_email = "powerstation_email"
+    powerstation_add_form_btn = "submit"
+    generator_add_form_btn = "submit"
+    generator_add_form_submit = "//form[@id='generator_add_form']//input[@id='submit']"
+
+    # Find by XPATH
+    powerstation_add_btn = "//img[@data-target='#powerstation_add']"
+    powerstation_table = '//div[@id="measurement_right"]'
+    generator_add_btn = "//img[@data-target='#generator_add']"
+    measurement_select_powerstation_form = "//select[@id='msaddform_powerstation']"
+    powerstation_list_check = "//table[@id = 'powerstation_list']//td[text()='" + Variables.powerstation_name + "']"
+    powerstation_list_number_testgen = "//table[@id='powerstation_list']//td[text()='" + Variables.powerstation_name + "']/following-sibling::td[3]/img"
+    generator_list_number_testgen = "//table[@id='table_generator_list']//td[text()='" + Variables.generator_name + "']/following-sibling::td[3]/img"
+    measurement_list_number_testgen = "//table[@id='measurement_table']//td[text()='" + Variables.measurement_date + "']/following-sibling::td[3]/img"
+
